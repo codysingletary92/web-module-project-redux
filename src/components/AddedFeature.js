d5d74051f72ef33f95cc7bde27ff7d9e2e98af2a
samuel singletary <codysingletary92@gmail.com>
2021-04-27 18:43:23 -0400

    finished MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { removeFeature } from '../actions/actions'
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={ () => props.removeFeature(props.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapDispatchToProps = dispatch => {
+  return {
+    removeFeature: feature => dispatch(removeFeature(feature))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AddedFeature)
